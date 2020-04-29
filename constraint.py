5bb8194f729632c9127f7ba1ede5d28d5d1223fc
JulienDona <61729724+JulienDona@users.noreply.github.com>
2020-04-29 22:58:53 +0200

    Update constraint.py

--- constraint.py
+++ constraint.py
@@ -2,20 +2,27 @@ import numpy as np
 
 """
 
-@param array; x parameters to find
-@param object ManualControllerFile; controller;  controller use in for the dynamical system
-@return mean squared error for these parameters
+@param array; position; positions of the ball during the simulation
+@param array; velocity; velocities of the ball during the simulation
+@param int; v; current control applied to the motor
+@return int; v; eventually modified control to apply on the motor
  """
-def idiot_proofing(position, velocity, v, active=True):  # FONCTION IDIOT PROOFING (GERE LA VITESSE DE LA BALL QUAND ELLE SE RAPPROCHE DES BORDS)
-    if (abs(position[-1]) >= 34):   # MOINS RESTIRCTIF, UN PEU BRUSQUE PARFOIS
+def idiot_proofing(position, velocity, v):
+    if (abs(position[-1]) > 31.5):
         p1 = 10
-        v_max = p1 / abs(position[-1])
-        # print("vmax", v_max)
-        # # print(velocity[-1])
+        v_max = p1 / (abs(position[-1]))
+        
         if (abs(velocity[-1]) > v_max):
             p2 = -100
-            a = p2*abs(1 / position[-1]) # en fonction de la position, pcq definir a en fonction de la v_max est inutile, puisque v_max depend de la position
-            v -= p2*np.deg2rad(np.tan((position[-1]-18*np.sign(position[-1]))*np.pi/80))
-        # v = - 3*v #méthode Adrien
-        # print("ok")
+         
+            if(abs(v) < 0.04):
+                v -= p2*np.deg2rad(np.tan((position[-1]-18*np.sign(position[-1]))*np.pi/80))
+                
+            else:
+                v = -2*v
+                
+
+    if(abs(v) > np.deg2rad(11)):
+        v = np.deg2rad(11*np.sign(v))
+      
     return v
